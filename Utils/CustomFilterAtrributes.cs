0169e12efb39377eb7da489b1dfa888f4c3e108c
Mateusz Matowicki <matowicki@gmail.com>
2021-06-14 22:17:37 +0200

    Add project files.

---
+++ Utils/CustomFilterAtrributes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Filters.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                var af = context.HttpContext.Connection.RemoteIpAddress.AddressFamily.ToString();
+                string ad = context.HttpContext.Connection.RemoteIpAddress.ToString();
+                page.ViewData["filterMessage"] = "Komunikat z CustomFilterAttributes!!! " + $"{af}" + $"{ad}";
+            }
+        }
+
+    }
+}
---
+++ Utils/CustomPageFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Utils
+{
+    public class CustomPageFilter : IAsyncPageFilter
+    {
+        public CustomPageFilter(IConfiguration _config)
+        {
+            int a = 0;
+        }
+        public void OnPageHandlerSelected(PageHandlerSelectedContext pageContext)
+        {
+            int a = 0;
+        }
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext pageContext)
+        {
+            int a = 0;
+        }
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext pageContext)
+        {
+            int a = 0;
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
